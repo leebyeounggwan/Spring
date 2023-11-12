3fc881230827cddd87afc71a6f8e4ccdbdaf5fae
leebyeounggwan <gwan0622@gmail.com>
2023-11-12 20:37:51 +0900

    create: Catalog-Service

---
+++ MSA/catalog-service/src/main/java/com/example/catalogservice/dto/CatalogDto.java
@@ -0,0 +1,19 @@
+package com.example.catalogservice.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class CatalogDto implements Serializable {
+    private String productId;
+    private Integer qty;
+    private Integer unitPrice;
+    private Integer totalPrice;
+
+    private String orderId;
+    private String userId;
+}
